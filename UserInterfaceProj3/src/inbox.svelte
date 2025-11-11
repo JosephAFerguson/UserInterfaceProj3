7b968ef24702bbbef6bf89c366e114d584a4fb74
Joe Ferguson <fergujp@mail.uc.edu>
2025-11-10 22:51:04 -0500

    initial boilerplates

---
+++ UserInterfaceProj3/src/inbox.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<div class="inbox-content">
+    <p>
+        This is the inbox page
+    </p>
+</div>
+
+<style>
+    .inbox-content {
+        display: grid;
+        grid-area: content;
+    }
+</style>
---
+++ UserInterfaceProj3/src/navbar.svelte
@@ -0,0 +1,23 @@
+<script>
+
+</script>
+
+<div class="navbar-content">
+    <a href="#/">Home</a>
+    <a href="#/inbox">Inbox</a>
+    <a href="#/bill">Billing</a>
+    <a href="#/settings">Settings</a>
+</div>
+
+<style>
+    .navbar-content {
+        display: flex;
+        grid-area: navbar-content;
+        background: var(--color-lm-bg2);
+        max-height:100%;
+        overflow: hidden;
+        align-items: center;
+        justify-content: center;
+        gap: 6rem;
+    }
+</style>
