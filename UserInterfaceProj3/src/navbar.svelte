f81bbc4252f2cafee07e4fa8d7427fea67f38fe5
Joe Ferguson <fergujp@mail.uc.edu>
2025-11-12 21:36:57 -0500

    colors and layout updates for navbar component

--- UserInterfaceProj3/src/navbar.svelte
+++ UserInterfaceProj3/src/navbar.svelte
@@ -3,21 +3,57 @@
 </script>
 
 <div class="navbar-content">
-    <a href="#/">Home</a>
-    <a href="#/inbox">Inbox</a>
-    <a href="#/bill">Billing</a>
-    <a href="#/settings">Settings</a>
+    <div class="navbar-home">  
+        <a href="#/">
+            <img src="logo.png" alt="Logo" width="30" height="30"/>
+            <p>Home</p>
+        </a>
+    </div>
+    <div class="navbar-inbox">  
+        <a href="#/">
+            <img src="logo.png" alt="Logo" width="30" height="30"/>
+            <p>Inbox</p>
+        </a>
+    </div>
+    <div class="navbar-bill">  
+        <a href="#/">
+            <img src="logo.png" alt="Logo" width="30" height="30"/>
+            <p>Billing</p>
+        </a>
+    </div>
+    <div class="navbar-settings"> 
+        <a href="#/"> 
+            <img src="logo.png" alt="Logo" width="30" height="30"/>
+            <p>Settings</p>
+        </a>
+    </div>
 </div>
 
 <style>
     .navbar-content {
         display: flex;
         grid-area: navbar-content;
-        background: var(--color-lm-bg2);
-        max-height:100%;
+        background: var(--color-lm-primary);
+        align-items: center;         
+        justify-content: center;      
         overflow: hidden;
-        align-items: center;
-        justify-content: center;
-        gap: 6rem;
+        gap: 12%;                 
+    }
+
+    .navbar-inbox, 
+    .navbar-home, 
+    .navbar-bill, 
+    .navbar-settings {
+        display: grid;
+        grid-template-rows: auto auto;
+        justify-content: center;     
+    }
+
+    .navbar-content a {
+        align-items: center;         
+        text-decoration: none;
+        color: var(--color-lm-on-primary);
+        font-size: 0.9rem;
+        font-weight: bold;
     }
 </style>
